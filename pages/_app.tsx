841b2700d568e363579401151ebd3740a3c71614
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-01-12 01:55:41 +0800

    created h component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,15 @@ import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <title>My-top is best top</title>
+    <link rel="icon" href="/favicon.ico"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+          rel="stylesheet"/>
+    <Component {...pageProps} /></>;
+    
 }
+
+
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from 'next/document';
+import React from 'react';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {...initialProps};
+  }
+
+  render(): React.JSX.Element {
+    return <Html lang={'ru'}>
+      <Head/>
       <body>
-        <Main />
-        <NextScript />
+      <Main/>
+      <NextScript/>
       </body>
-    </Html>
-  )
+    </Html>;
+  }
 }
