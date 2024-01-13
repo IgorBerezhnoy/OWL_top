b0a8e941e741128ee34e70987f18aa196950c535
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-01-13 23:23:22 +0800

    created button arrow

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -2,12 +2,18 @@ import React, {JSX} from 'react';
 import {ButtonType} from '@/components/button/button.props';
 import s from './button.module.css';
 import classNames from 'classnames';
+import {Vector} from '@/components/button/index';
 
-export const Button = ({children, appearance, className, ...rest}: ButtonType): JSX.Element => {
+export const Button = ({arrow = 'none', children, appearance, className, ...rest}: ButtonType): JSX.Element => {
 
   return <button {...rest} className={classNames(s.button, className, {
     [s.primary]: appearance == 'primary',
     [s.ghost]: appearance == 'ghost'
-  })}>{children}</button>;
+  })}>{children}{arrow !== 'none' &&
+    <span className={classNames(s.arrow, {[s.down]: arrow === 'down'})}>
+
+     <Vector/>
+
+    </span>}</button>;
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@ import {Inter} from 'next/font/google';
 import {Htag} from '@/components/Htag/Htag';
 import {Button} from '@/components/button/button';
 
-const inter = Inter({subsets: ['latin']});
+// const inter = Inter({subsets: ['latin']});
 
 export default function Home() {
   return (
     <div>
+
+      <Button appearance={'primary'} arrow={"right"}>primary</Button>
+      <Button appearance={'primary'} arrow={"down"}>primary</Button>
       <Button appearance={'primary'}>primary</Button>
       <Button onClick={() => console.log('ghost')} appearance={'ghost'}>ghost</Button>
       <Htag tag={'h1'}>aaaaassaaa</Htag>
